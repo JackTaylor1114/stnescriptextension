88ef8a1af51ef1fbcc64cf0e454c385bcf78db71
unknown <tobias.grimm.f5o@fh-zwickau.de>
2021-04-10 14:52:24 +0200

    Updated code documentation

--- src/extension.ts
+++ src/extension.ts
@@ -1,74 +1,83 @@
 'use strict';
 
+//Imports
 import * as vscode from 'vscode';
 import * as data from './stne.json';
 
+//Definitions
 let beautify_js = require('js-beautify');
 let types: Array<Type> = [];
-let editor;
-const errormarker = vscode.window.createTextEditorDecorationType({
-    overviewRulerColor: 'red',
-    overviewRulerLane: vscode.OverviewRulerLane.Right,
-    textDecoration: 'wavy underline red'
-});
 
 /**
  * Main function that is called when the extension is started
  */
 export function activate(context: vscode.ExtensionContext) {
 
+    //Load the types from the stne.json file and store them 
     LoadTypesFromJSON();
 
-    //Register DocumentFormattingEditProvider
+    //Register DocumentFormattingEditProvider (Formats the whole document)
     vscode.languages.registerDocumentFormattingEditProvider('stnescript-lang', {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+
+            //Load the whole document text
             let documentText = document.getText();
             let documentStart: vscode.Position = document.lineAt(0).range.start;
             let documentEnd: vscode.Position = document.lineAt(document.lineCount - 1).range.end;
             let documentFullRange: vscode.Range = new vscode.Range(documentStart, documentEnd);
+
+            //Format the document using the JavaScript librarby 
             let documentFormatted = beautify_js(documentText, { indent_size: 4, space_in_empty_paren: true });
+
+            //Do some custom changes after the formatting
             documentFormatted = documentFormatted.replace("< >", "<>");
+
+            //Return the formatted text
             return [vscode.TextEdit.replace(documentFullRange, documentFormatted)];
         }
     });
 
-    //Register CompletitionItemsProvider when user types a '.'
+    //Register CompletitionItemsProvider (Provides suggestions when user types a dot)
     vscode.languages.registerCompletionItemProvider('stnescript-lang', {
         provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+
+            //Get the text written between the dot and and the last space in the line
             const linePrefix = document.lineAt(position).text.substr(0, position.character);
             let docText = document.getText();
             let wordsOfLine = linePrefix.split(' ');
             let lastWord = wordsOfLine[wordsOfLine.length - 1];
             lastWord = lastWord.slice(0, -1);
+
+            //Return the completition suggestions
             return GetCompletitionItemsForType(GetTypeForSuspectedVar(docText, lastWord));
         }
     },
         '.'
     );
 
-    //Register CompletitionItemsProvider when user types a ' '
+    //Register CompletitionItemsProvider (Provides suggestions when user types a space)
     vscode.languages.registerCompletionItemProvider('stnescript-lang', {
         provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+
+            //Get the text written between the space and and the last space in the line
             const linePrefix = document.lineAt(position).text.substr(0, position.character);
             let wordsOfLine = linePrefix.split(' ');
             let word = wordsOfLine[wordsOfLine.length - 2];
-            if (word == "New" || word == "As")
-                return GetTypeCompletitionItems();
-            return undefined;
+
+            //Check if the last word was "New" or "As" 
+            if (word != "New" && word != "As")
+                return undefined;
+
+            //Return the completition suggestions
+            return GetTypeCompletitionItems();
         }
     },
         ' '
     );
-
-    editor = vscode.window.activeTextEditor;
-
-    vscode.workspace.onDidChangeTextDocument(e => {
-        checkForErrors();
-    }, null, context.subscriptions);
 }
 
 /**
- * Read available types from a JSON file and store them
+ * Read available types from the stne.json file and store them
  */
 function LoadTypesFromJSON() {
     for (let type of data.types) {
@@ -89,8 +98,12 @@ function LoadTypesFromJSON() {
  * Returns the name of a type for a passed variable name
  */
 function GetTypeForSuspectedVar(text: string, term: string): Type {
+
+    //Match the document text to check if the text contains a definition with the variable name
     const regex = new RegExp("Var\\s(" + term + ")\\sAs\\s([Nn]ew\\s)?([a-zA-Z]*)");
     let match = text.match(regex);
+
+    //Check if a match was found
     if (IsNull(match))
         return undefined;
     let name = match[3];
@@ -99,6 +112,8 @@ function GetTypeForSuspectedVar(text: string, term: string): Type {
     let type = types.find(t => t.name == name);
     if (IsNull(type))
         return undefined;
+
+    //Return the type of the variable 
     return type;
 }
 
@@ -106,6 +121,8 @@ function GetTypeForSuspectedVar(text: string, term: string): Type {
  * Returns all available types as completition items
  */
 function GetTypeCompletitionItems(): Array<vscode.CompletionItem> {
+
+    //Return all known types
     let items: Array<vscode.CompletionItem> = [];
     for (let type of types)
         items.push(new vscode.CompletionItem(type.name, vscode.CompletionItemKind.Class))
@@ -116,16 +133,23 @@ function GetTypeCompletitionItems(): Array<vscode.CompletionItem> {
  * Returns completition items for a type (methods/properties)
  */
 function GetCompletitionItemsForType(type: Type): Array<vscode.CompletionItem> {
+
+    //Go trouth all known members of the type
     let items: Array<vscode.CompletionItem> = [];
     for (let member of type.members) {
+
+        //If the member is an attribute, add the attribute name and type to the results
         let item: vscode.CompletionItem;
         if (member.membertype == "Property" || member.membertype == "Field") {
             item = new vscode.CompletionItem(member.name, vscode.CompletionItemKind.Property);
             item.detail = member.name + ":" + member.type;
         }
+        //If the member is a method, add the method to the results
         else {
             item = new vscode.CompletionItem(member.name, vscode.CompletionItemKind.Method);
             item.detail = member.name + "(";
+
+            //If the method has any parameters, add them to the item
             if (member.params.length > 0) {
                 for (let param of member.params)
                     item.detail = item.detail + param.name + ":" + param.type + ",";
@@ -133,46 +157,15 @@ function GetCompletitionItemsForType(type: Type): Array<vscode.CompletionItem> {
             }
             item.detail = item.detail + ")";
         }
+
+        //If the member is static, add "(static) to its description"
         if (member.stat)
             item.detail = item.detail + " (static)";
         items.push(item)
     }
+
+    //Return all completition items
     return items;
-}
-
-/**
- * Checks for various erros and decorates them in the text editor
- */
-function checkForErrors() {
-    let text = vscode.window.activeTextEditor.document.getText();
-    let errordecorations: vscode.DecorationOptions[] = [];
-    checkForDeclarationsExample(text, errordecorations);
-    editor.setDecorations(errormarker, errordecorations);
-    return errordecorations;
-}
-
-/**
- * Searches for variable decorations
- */
-function checkForDeclarationsExample(text: string, errordecorations: vscode.DecorationOptions[]) {
-    const regex = new RegExp("Var\\s[a-zA-Z]*\\sAs\\s[Nn]ew\\s?[a-zA-Z]*");
-    let firstLineNumber = null;
-    let activeEditor = vscode.window.activeTextEditor;
-    let match = text.match(regex);
-    while (match) {
-        if (firstLineNumber == null) {
-            firstLineNumber = activeEditor.document.positionAt(match.index).line + 1;
-        }
-        const startPos = activeEditor.document.positionAt(match.index);
-        const endPos = activeEditor.document.positionAt(match.index + match[0].length);
-        const decoration = {
-            'range': new vscode.Range(startPos, endPos),
-            'hoverMessage': "This is a declaration!"
-        };
-        errordecorations.push(decoration);
-        text = text.replace(match[0],"");
-        match = text.match(regex);
-    }
 }
 
 /**
